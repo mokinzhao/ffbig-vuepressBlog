c01a6faa0daa8f08dde23e88023e3dd7e41ca0a9
mokinzhao <yl.zhao@neoclub.cn>
2019-09-24 17:23:02 +0800

    fix style

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+### 
+# @Author: mokinzhao
+ # @Date: 2019-09-24 16:56:20
+ # @Description: 
+ # @LastEditTime: 2019-09-24 16:59:48
+ ###
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.yourwebsite.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:mokinzhao/mokinzhao.github.io.git master
+
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+
+cd -
