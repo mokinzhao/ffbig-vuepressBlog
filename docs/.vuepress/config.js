057723ef61f7cf771c4a4b8fd19e081c3bda384e
mokinzhao <37622852@qq.com>
2019-11-17 13:09:37 +0800

    update：面试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -88,7 +88,7 @@ module.exports = {
           },
           {
             text: "数据结构与算法",
-            link: "https://mp.weixin.qq.com/s/EFil0A_ylv_PCreVj-SebA"
+            link: "http://www.conardli.top/docs/dataStructure/"
           },
           {
             text: "设计模式",
@@ -98,24 +98,24 @@ module.exports = {
       },
       { text: "博客", link: "https://blog.csdn.net/z4909801" }, // 外部链接
       // 下拉列表
-      {
-        text: "他山之石",
-        items: [
-          { text: "木易杨前端进阶", link: "https://www.muyiy.cn/" },
-          {
-            text: "数据结构与算法",
-            link: "http://www.conardli.top/docs/"
-          },
-          {
-            text: "前端面试与进阶指南",
-            link: "https://www.cxymsg.com/"
-          },
-          {
-            text: "前端进阶之道",
-            link: "https://yuchengkai.cn/"
-          }
-        ]
-      }
+      // {
+      //   text: "他山之石",
+      //   items: [
+      //     { text: "木易杨前端进阶", link: "https://www.muyiy.cn/" },
+      //     {
+      //       text: "数据结构与算法",
+      //       link: "http://www.conardli.top/docs/"
+      //     },
+      //     {
+      //       text: "前端面试与进阶指南",
+      //       link: "https://www.cxymsg.com/"
+      //     },
+      //     {
+      //       text: "前端进阶之道",
+      //       link: "https://yuchengkai.cn/"
+      //     }
+      //   ]
+      // }
     ]
   }
 };
