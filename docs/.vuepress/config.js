a2ba23dbf6380d27fd93479d6f4e344dd02169b3
mokinzhao <37622852@qq.com>
2021-04-19 00:16:52 +0800

    update：link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: mokinzhao
  * @Date: 2019-09-24 15:02:43
  * @Description:
- * @LastEditTime : 2019-12-20 11:00:28
+ * @LastEditTime: 2021-04-19 00:16:25
  */
 module.exports = {
   title: "全栈大前端",
@@ -24,12 +24,12 @@ module.exports = {
       var s = document.getElementsByTagName("script")[0]; 
       s.parentNode.insertBefore(hm, s);
     })();
-`
-    ]
+`,
+    ],
   ],
   base: "/", // 这是部署到github相关的配置 下面会讲
   markdown: {
-    lineNumbers: true // 代码块显示行号
+    lineNumbers: true, // 代码块显示行号
   },
   plugins: ["autobar"],
   themeConfig: {
@@ -62,107 +62,124 @@ module.exports = {
         items: [
           {
             text: "框架设计：PC 端单页多页框架如何设计与落地 ",
-            link: "https://mp.weixin.qq.com/s/m519w-RtdslHHrcbi_VLBA"
+            link: "https://mp.weixin.qq.com/s/m519w-RtdslHHrcbi_VLBA",
           },
           {
             text: "框架设计：RN 端的框架如何设计与落地",
-            link: "https://mp.weixin.qq.com/s/Vtczty5i8awo3p7qspjhmg"
+            link: "https://mp.weixin.qq.com/s/Vtczty5i8awo3p7qspjhmg",
           },
           {
             text: "透彻分析：常见的前端架构风格和案例",
-            link: "https://mp.weixin.qq.com/s/x4KcDPrV2aqtjkRdow_JVg"
+            link: "https://mp.weixin.qq.com/s/x4KcDPrV2aqtjkRdow_JVg",
           },
-
           {
             text: "领域驱动设计在前端中的应用",
-            link: "https://mp.weixin.qq.com/s/1kl8SV28GDt_40FF_6CuhA"
-          }
-        ]
+            link: "https://mp.weixin.qq.com/s/1kl8SV28GDt_40FF_6CuhA",
+          },
+          {
+            text: "大型项目前端架构浅谈",
+            link: "https://juejin.cn/post/6844903853859536903",
+          },
+        ],
       },
       {
         text: "数据结构与算法",
         items: [
           {
             text: "一文概览数据结构与算法",
-            link: "https://mp.weixin.qq.com/s/EFil0A_ylv_PCreVj-SebA"
+            link: "https://mp.weixin.qq.com/s/EFil0A_ylv_PCreVj-SebA",
           },
           {
-            text: "数据结构与算法系列",
-            link: "http://www.conardli.top/docs/dataStructure/"
+            text: "数据结构与算法必会的50个代码实现",
+            link: "https://github.com/wangzheng0822/algo",
           },
           {
-            text: "十大经典排序",
-            link: "https://segmentfault.com/a/1190000019916376"
-          }
-        ]
+            text: "算法专栏",
+            link: "http://www.conardli.top/docs/dataStructure/",
+          },
+          {
+            text: "力扣加加",
+            link:
+              "https://leetcode-solution-leetcode-pp.gitbook.io/leetcode-solution/",
+          },
+          {
+            text: "labuladong的算法小抄",
+            link: "https://labuladong.github.io/algo/",
+          },
+        ],
       },
 
       {
-        text: "网络与浏览器缓存",
+        text: "低码平台",
         items: [
           {
-            text: "HTTP协议基础概述",
-            link: "/网络/Http"
+            text: "H5-dooring",
+            link: "http://h5.dooring.cn/",
           },
-          {
-            text: "TCP协议与OSI 七层模型",
-            link: "/网络/Tcp"
-          },
-          {
-            text: "解读HTTP/2与HTTP/3 的新特性",
-            link: "https://mp.weixin.qq.com/s/zhYWDhsqrBO5MB4Hw2XkDA"
-          },
-          {
-            text: "从URL输入到页面展现到底发生什么",
-            link: "https://mp.weixin.qq.com/s/mzckXvsqW99xe_t-uUYy4w"
-          }
-        ]
+        ],
       },
 
       {
-        text: "面向对象与设计模式",
+        text: "编程范式与设计模式",
         items: [
+          {
+            text: "编写高质量可维护的代码：编程范式",
+            link: "https://www.zoo.team/article/program-paradigm",
+          },
           {
             text: "面向对象之三个基本特征（javaScript）",
-            link: "https://segmentfault.com/a/1190000018239556"
+            link: "https://segmentfault.com/a/1190000018239556",
           },
           {
             text: "面向对象之七大基本原则（javaScript）",
-            link: "https://segmentfault.com/a/1190000020319171"
+            link: "https://segmentfault.com/a/1190000020319171",
           },
           {
-            text: "设计模式",
-            link: "https://juejin.im/post/5c2e10a76fb9a049c0432697"
-          }
-        ]
+            text: "设计模式与开发实践",
+            link: "https://juejin.im/post/5c2e10a76fb9a049c0432697",
+          },
+          {
+            text: "前端精读-设计模式",
+            link:
+              "https://github.com/ascoders/weekly/tree/master/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F",
+          },
+        ],
       },
       {
         text: "跨端融合",
         items: [
           {
-            text: "ReactNative",
-            link: "https://facebook.github.io/react-native/"
+            text: "深入浅出主流的几款小程序跨端框架原理",
+            link: "https://juejin.cn/post/6881597846307635214",
           },
           {
-            text: "Flutter",
-            link: "https://flutter.dev/"
+            text: "去哪儿网React Native 优先的多端统一化方案",
+            link: "https://juejin.cn/post/6893028483232956429",
+          },
+          {
+            text: "Electron",
+            link: "https://www.electronjs.org/docs",
           },
           {
             text: "Taro",
-            link: "https://nervjs.github.io/taro/"
+            link: "https://nervjs.github.io/taro/",
           },
           {
             text: "Uni-app",
-            link: "https://uniapp.dcloud.io/"
+            link: "https://uniapp.dcloud.io/",
+          },
+          {
+            text: "Flutter",
+            link: "https://flutter.dev/",
           },
           {
             text: "PWA",
             link:
-              "https://developer.mozilla.org/zh-CN/docs/Web/Progressive_web_apps"
-          }
-        ]
+              "https://developer.mozilla.org/zh-CN/docs/Web/Progressive_web_apps",
+          },
+        ],
       },
-      { text: "博客", link: "https://blog.csdn.net/z4909801" } // 外部链接
+      { text: "博客", link: "https://blog.csdn.net/z4909801" }, // 外部链接
       // 下拉列表
       // {
       //   text: "他山之石",
@@ -182,6 +199,6 @@ module.exports = {
       //     }
       //   ]
       // }
-    ]
-  }
+    ],
+  },
 };
